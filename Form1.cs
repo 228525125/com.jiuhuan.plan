dc13f2abe260db662fd875bf392fc277a5b5e38d
chenxian <228525125@users.noreply.github.com>
2026-07-24 12:02:03 +0800

    更新 Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -24,6 +24,7 @@ namespace com.jiuhuan.plan.view
             InitializeComponent();
             ConfigHelper.Init();
             int i = 0;
+            string ss = "123";
         }
 
         private void Form1_Load(object sender, EventArgs e)
